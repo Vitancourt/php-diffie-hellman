b0edfd390f1a994e3a6f790e531264acc4a18193
Maikel <maikel-93@hotmail.com>
2018-09-10 23:13:38 -0300

    Fim

--- diffiehellman.php
+++ diffiehellman.php
@@ -2,18 +2,25 @@
 //ini_set('precision', 20);
 session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   $a = $_POST["a"];
-   $x1 = $_POST["x1"];
-   $b = $_POST["b"];
-   $y1 = $_POST["y1"];
-   $x2 = ($a**$x1)%$b;   
-   $y2 = ($a**$y1)%$b;
-   $teste0 = ($x2**$y1)%$b;
-   $teste1 = ($y2**$x1)%$b;
-
-   if ($teste0 == $teste1) {
-      echo "Z = ".$teste0;
-   }
+    $a = $_POST["a"];
+    $x1 = $_POST["x1"];
+    $b = $_POST["b"];
+    $y1 = $_POST["y1"];
+    $x2 = ($a**$x1)%$b;  
+    echo "X1 = ".$x1;
+    echo "<br>";
+    echo "X2 = ".$x2;
+    $y2 = ($a**$y1)%$b;
+    echo "<br>";
+    echo "Y1 = ".$x1;
+    echo "<br>";
+    echo "Y2 = ".$x2;
+    $teste0 = ($x2**$y1)%$b;
+    $teste1 = ($y2**$x1)%$b;
+    if ($teste0 == $teste1) {
+        
+        echo "<br>Z = ".$teste0;
+    }
 }
 ?>
 <form method="post" action="<?=$_SERVER["PHP_SELF"];?>">
